a416fc8974a510261bd5f21620d5bc03ace8e0d2
James <jjfeore>
2017-06-05 11:44:11 -0700

    Added all the rests of the graph methods

--- src/graph_1.py
+++ src/graph_1.py
@@ -3,14 +3,17 @@
 
 class Graph(object):
     """Define graph and modules."""
+
     def __init__(self):
+        """Instantiate a new graph."""
         self.graph = {}
 
     def nodes(self):
+        """Return a list of all the nodes in the graph."""
         return list(self.graph.keys())
 
     def edges(self):
-        """Lists the edges."""
+        """List the edges."""
         edges = []
         for node in self.graph:
             for i in self.graph[node]:
@@ -18,12 +21,12 @@ class Graph(object):
         return edges
 
     def add_node(self, x):
-        """Adds a node with a iterable which is a neighbor of x."""
+        """Add a node with a iterable which is a neighbor of x."""
         if not self.graph[x]:
             self.graph[x] = []
 
     def add_edge(self, val1, val2):
-        """Adds a edge to two values."""
+        """Add an edge between two values."""
         if not self.graph[val1]:
             self.graph[val1] = []
         if not self.graph[val2]:
@@ -32,16 +35,35 @@ class Graph(object):
             self.graph[val1].append(val2)
 
     def del_node(self, node):
-        """Removes node from graph."""
-        self.graph.pop(node)
+        """Remove node from graph."""
+        if self.graph.pop(node):
+            for a_node in self.graph:
+                if node in self.graph[a_node]:
+                    self.graph[a_node].remove(node)
+
+    def has_node(self, val):
+        """Check to see if a specified node exists in the graph. Return a bool."""
+        if val in self.graph:
+            return True
+        return False
 
     def del_edge(self, val1, val2):
+        """Delete the edge between two nodes."""
         if val2 in self.graph[val1]:
             del self.graph[val1][val2]
 
-    def neighbors(self):
-        
+    def adjacent(self, val1, val2):
+        """Return a list ."""
+        if not self.has_node(val1) or not self.has_node(val2):
+            raise ValueError('Graph does not contain one of the nodes')
+        if val2 in self.graph[val1]:
+            return True
+        else:
+            return False
 
-    def adjacent(val1, val2):
-       if val1 in self.graph[val2]:
-        return True
+    def neighbors(self, val):
+        """Return a list all of a node's neighbors."""
+        try:
+            return self.graph[val]
+        except KeyError:
+            raise KeyError('{} not in the graph.'.format(val))
